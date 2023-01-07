413643f4322d9b49ff810bda25e8ad88e8bdb629
Chandramohan97 <chandramsriv@gmail.com>
2023-01-07 20:07:14 +0530

    adding assets router to index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const app = require("express")();
 const port = 8080;
 const loginRouter = require("./router/loginRouter");
 const signUpRouter = require("./router/signUpRouter");
+const assets = require("./router/asset");
 
 app.use("/signUp", signUpRouter);
 app.use("/login", loginRouter);
+app.use("/assets", assets);
 
 app.get("/", (req, res) => {
   res.send("Server working");
