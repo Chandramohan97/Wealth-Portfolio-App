2816439964ab7b2d675b956c0ca5e3ff742b826c
Chandramohan97 <chandramsriv@gmail.com>
2023-01-08 12:16:41 +0530

    assetUpdate : adding the API to update fields in documents.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const port = 8080;
 const loginRouter = require("./router/loginRouter");
 const signUpRouter = require("./router/signUpRouter");
 const assets = require("./router/asset");
+const assetUpdate = require("./router/assetUpdate");
 
 app.use("/signUp", signUpRouter);
 app.use("/login", loginRouter);
 app.use("/assets", assets);
+app.use("/assetUpdate", assetUpdate);
 
 app.get("/", (req, res) => {
   res.send("Server working");
